8d2f5238f9bbb5b6b6817ae2f005ecf4c6f2cc7a
Klas Wong-In Andersson <kw222tw@student.lnu.se>
2025-10-02 01:04:14 +0200

    Refactor matrix creation to be more object oriented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { validateInput } from "./validator.js";
 import { buildDataCodewords } from "./datastream.js";
 import { codewordsToBits } from "./encoder.js";
-import { buildMatrix } from "./matrix.js";
+import { QRMatrix } from "./QRMatrix.js";
 import { QRRenderer } from "./QRRenderer.js";
 
 const qrRenderer = new QRRenderer();
@@ -11,14 +11,13 @@ export function generateQRCode(text, options) {
 
   const codewords = buildDataCodewords(text, options);
   const bits = codewordsToBits(codewords);
-  const matrix = buildMatrix(bits);
+  const matrix = new QRMatrix().build(codewords, options.maskPattern || 0);
   return matrix;
 }
 
 export { validateInput } from "./validator.js";
 export { buildDataCodewords } from "./datastream.js";
 export { codewordsToBits } from "./encoder.js";
-export { buildMatrix } from "./matrix.js";
 
 const matrix = generateQRCode("Hello, World!", { mode: "byte" });
 const asciiArt = qrRenderer.renderASCII(matrix);
