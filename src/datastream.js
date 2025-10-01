22d3f7fa9122caf85c1c6eeaa344eb6895e96f5f
Klas Wong-In Andersson <kw222tw@student.lnu.se>
2025-10-02 01:22:16 +0200

    Refactor code for datastream bits to be more object oriented

--- src/datastream.js
+++ src/datastream.js
@@ -14,100 +14,95 @@ import {
   PAD_BYTES,
 } from "./constants.js";
 
-// Builds data codewords for QR Version 1 using byte mode
-// Structure: mode indicator + char count + data + terminator + padding
-
-const MAX_BITS = MAX_DATA_CODEWORDS * BITS_PER_CODEWORD;
-
-export function buildDataCodewords(text, { mode = "byte" } = {}) {
-  validateMode(mode);
-
-  const dataBits = createDataBits(text);
-  const paddedBits = addPadding(dataBits);
-  return convertBitsToCodewords(paddedBits);
-}
-
-// Creates: mode(4) + count(8) + data(variable)
-function createDataBits(text) {
-  const byteData = stringToBytes(text);
-  const bits = [];
-
-  bits.push(...createModeIndicator());
-  bits.push(...createCharacterCount(byteData.length));
-  bits.push(...createDataSection(byteData));
-
-  return bits;
-}
-
-// Adds terminator and pads to byte boundary
-function addPadding(dataBits) {
-  const bits = [...dataBits];
-
-  addTerminatorBits(bits);
-  addBytePadding(bits);
-
-  return bits;
-}
-
-// Groups 8 bits into codewords, adds pad codewords if needed
-function convertBitsToCodewords(bits) {
-  const codewords = [];
-
-  for (let i = 0; i < bits.length; i += BITS_PER_CODEWORD) {
-    const codewordBits = bits.slice(i, i + BITS_PER_CODEWORD);
-    const codeword = parseInt(codewordBits.join(""), 2);
-    codewords.push(codeword);
+export class DataStream {
+  constructor(text, options = {}) {
+    this.text = text;
+    this.mode = options.mode || "byte";
+    this.maxBits = MAX_DATA_CODEWORDS * BITS_PER_CODEWORD;
   }
 
-  addPadCodewords(codewords);
+  build() {
+    this.#validateMode();
 
-  return codewords;
-}
-
-function validateMode(mode) {
-  if (mode !== "byte") {
-    throw new Error("Only byte mode is supported in this implementation.");
+    const dataBits = this.#createDataBits();
+    const paddedBits = this.#addPadding(dataBits);
+    return this.#convertBitsToCodewords(paddedBits);
   }
-}
 
-function createModeIndicator() {
-  return numberToBits(BYTE_MODE_INDICATOR, BYTE_MODE_BITS);
-}
-
-function createCharacterCount(length) {
-  return numberToBits(length, CHAR_COUNT_BITS);
-}
-
-function createDataSection(byteData) {
-  const bits = [];
-  for (const byte of byteData) {
-    bits.push(...byteToBits(byte));
+  #validateMode() {
+    if (this.mode !== "byte") {
+      throw new Error("Only byte mode is supported in this implementation.");
+    }
   }
-  return bits;
-}
 
-// Add 0000 terminator (up to 4 bits)
-function addTerminatorBits(bits) {
-  const terminatorLength = Math.min(
-    TERMINATOR_MAX_BITS,
-    MAX_BITS - bits.length
-  );
-  bits.push(...Array(terminatorLength).fill(0));
-}
+  #createDataBits() {
+    const byteData = stringToBytes(this.text);
+    const bits = [];
 
-// Pad with zeros to reach byte boundary
-function addBytePadding(bits) {
-  while (bits.length % BITS_PER_CODEWORD !== 0) {
-    bits.push(0);
+    bits.push(...this.#createModeIndicator());
+    bits.push(...this.#createCharacterCount(byteData.length));
+    bits.push(...this.#createDataSection(byteData));
+
+    return bits;
   }
-}
 
-// Fill with alternating 11101100 00010001 pattern
-function addPadCodewords(codewords) {
-  let padIndex = 0;
+  #addPadding(dataBits) {
+    const bits = [...dataBits];
 
-  while (codewords.length < MAX_DATA_CODEWORDS) {
-    codewords.push(PAD_BYTES[padIndex % PAD_BYTES.length]);
-    padIndex++;
+    this.#addTerminatorBits(bits);
+    this.#addBytePadding(bits);
+
+    return bits;
+  }
+
+  #convertBitsToCodewords(bits) {
+    const codewords = [];
+
+    for (let i = 0; i < bits.length; i += BITS_PER_CODEWORD) {
+      const codewordBits = bits.slice(i, i + BITS_PER_CODEWORD);
+      const codeword = parseInt(codewordBits.join(""), 2);
+      codewords.push(codeword);
+    }
+
+    this.#addPadCodewords(codewords);
+    return codewords;
+  }
+
+  #createModeIndicator() {
+    return numberToBits(BYTE_MODE_INDICATOR, BYTE_MODE_BITS);
+  }
+
+  #createCharacterCount(length) {
+    return numberToBits(length, CHAR_COUNT_BITS);
+  }
+
+  #createDataSection(byteData) {
+    const bits = [];
+    for (const byte of byteData) {
+      bits.push(...byteToBits(byte));
+    }
+    return bits;
+  }
+
+  #addTerminatorBits(bits) {
+    const terminatorLength = Math.min(
+      TERMINATOR_MAX_BITS,
+      this.maxBits - bits.length
+    );
+    bits.push(...Array(terminatorLength).fill(0));
+  }
+
+  #addBytePadding(bits) {
+    while (bits.length % BITS_PER_CODEWORD !== 0) {
+      bits.push(0);
+    }
+  }
+
+  #addPadCodewords(codewords) {
+    let padIndex = 0;
+    while (codewords.length < MAX_DATA_CODEWORDS) {
+      codewords.push(PAD_BYTES[padIndex % PAD_BYTES.length]);
+      padIndex++;
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { validateInput } from "./validator.js";
-import { buildDataCodewords } from "./datastream.js";
+import { DataStream } from "./DataStream.js";
 import { codewordsToBits } from "./encoder.js";
 import { QRMatrix } from "./QRMatrix.js";
 import { QRRenderer } from "./QRRenderer.js";
@@ -9,14 +9,14 @@ const qrRenderer = new QRRenderer();
 export function generateQRCode(text, options) {
   validateInput(text, options);
 
-  const codewords = buildDataCodewords(text, options);
+  const dataStream = new DataStream(text, options);
+  const codewords = dataStream.build();
   const bits = codewordsToBits(codewords);
   const matrix = new QRMatrix().build(codewords, options.maskPattern || 0);
   return matrix;
 }
 
 export { validateInput } from "./validator.js";
-export { buildDataCodewords } from "./datastream.js";
 export { codewordsToBits } from "./encoder.js";
 
 const matrix = generateQRCode("Hello, World!", { mode: "byte" });
