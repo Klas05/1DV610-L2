3cdb32005203dea97ccb77799e14f2716eded39d
Klas Wong-In Andersson <kw222tw@student.lnu.se>
2025-11-22 20:33:42 +0100

    Refactor conversion utilities for improved clarity and consistency

--- src/conversionUtils.js
+++ src/conversionUtils.js
@@ -1,25 +1,26 @@
 import { BITS_PER_BYTE, BINARY_BASE } from "./constants.js";
 
-// Basic encoding utilities for converting text and numbers to bits
-
 export function stringToBytes(text) {
-  const encoder = new TextEncoder(); // UTF-8 encoding
+  const encoder = new TextEncoder();
   return encoder.encode(text);
 }
 
 export function codewordsToBits(codewords) {
-  const bits = [];
+  const allBits = [];
   for (const codeword of codewords) {
-    bits.push(...byteToBits(codeword));
+    allBits.push(...byteToBits(codeword));
   }
-  return bits;
+  return allBits;
 }
 
 export function byteToBits(byte) {
   return numberToBits(byte, BITS_PER_BYTE);
 }
 
-// Convert number to binary array with specified length
-export function numberToBits(num, length) {
-  return num.toString(BINARY_BASE).padStart(length, "0").split("").map(Number);
+export function numberToBits(number, bitLength) {
+  return number
+    .toString(BINARY_BASE)
+    .padStart(bitLength, "0")
+    .split("")
+    .map(Number);
 }
